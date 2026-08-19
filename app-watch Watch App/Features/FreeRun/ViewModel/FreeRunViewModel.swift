2dac42fee43215ca7e9900c327758c6f07e4f443
Débora_Ferreira <deboracris29a@gmail.com>
2026-08-19 15:15:01 -0300

    feat:  create folder structure

---
+++ app-watch Watch App/Features/FreeRun/ViewModel/FreeRunViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FreeRunViewModel.swift
+//  app-watch Watch App
+//
+//  Created by Débora Cristina Silva Ferreira on 19/08/26.
+//
+
+import Foundation
---
+++ app-watch Watch App/Features/GuideRun/ViewModel/GuideRunViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  ViewModel.swift
+//  app-watch
+//
+//  Created by Débora Cristina Silva Ferreira on 19/08/26.
+//
+
