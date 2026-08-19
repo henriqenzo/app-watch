3e834c8cbfbf43997576fa17065c6d74330e5e8d
FilipiNyetz <romao.martins.filipi@gmail.com>
2026-08-19 17:26:17 -0300

    feat/Add Design System

---
+++ app-watch Watch App/Utils/Shared/AppText.swift
@@ -0,0 +1,19 @@
+//
+//  AppText.swift
+//  app-watch Watch App
+//
+//  Created by Filipi Romão on 19/08/26.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct AppTypography {
+    static let heroText: Font = .system(size: 56, weight: .bold)
+    static let largeTitle: Font = .system(size: 34, weight: .bold)
+    static let title1: Font = .system(size: 28, weight: .bold)
+    static let title3: Font = .system(size: 20, weight: .semibold)
+    static let subheadline: Font = .system(size: 15, weight: .regular)
+    static let caption: Font = .system(size: 12, weight: .regular)
+}
