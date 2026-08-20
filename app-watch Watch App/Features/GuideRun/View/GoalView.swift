cea2708646dcec91f92e29dda07240af99d2e8c9
Débora_Ferreira <deboracris29a@gmail.com>
2026-08-20 10:11:50 -0300

    feat(goal-view): add goal view

---
+++ app-watch Watch App/Features/GuideRun/View/GoalView.swift
@@ -0,0 +1,19 @@
+//
+//  GoalView.swift
+//  app-watch Watch App
+//
+//  Created by Débora Cristina Silva Ferreira on 20/08/26.
+//
+
+import Foundation
+import SwiftUI
+
+struct GoalView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    GoalView()
+}
